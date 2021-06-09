aee4367096d57f98bb86c6ede464816131049809
Yanin Tuamsuk <khame@jitta.com>
2021-06-09 11:45:23 +0700

    add locale

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
+const withPlugins = require('next-compose-plugins')
 const withTM = require("next-transpile-modules")(["components"]);
-module.exports = withTM();
+
+module.exports = withPlugins([[withTM]], {
+  i18n: {
+    locales: ['th', 'en'],
+    defaultLocale: 'th',
+    localeDetection: false
+  }
+})
